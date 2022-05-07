7d36ac44d064e3c7352a82261c1068447e68c2af
Rupasappati <rupasappati@gmail.com>
2022-05-07 14:56:16 +0530

    Added: Percentage of head & tails

---
+++ FlipCoin/Flip_Coin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    public class Flip_Coin
+    {
+        double heads = 0;
+        double Tails = 0;
+        Random number = new Random();
+
+        public void Heads_Tails()
+        {
+            
+            Console.WriteLine("Please enter the no.of Flips of the coin");
+            double NumOfFlips = Convert.ToInt32(Console.ReadLine());
+
+            if (NumOfFlips > 0)
+            {
+                for (int i = 0; i < NumOfFlips; i++)
+                {
+                    int coin = number.Next(1, 3);
+                    if (coin == 1)
+                    {
+                        Console.WriteLine("User get Heads");
+                        heads++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("user get Tails");
+                        Tails++;
+                    }
+                }
+
+                double Percent_heads = Math.Round((heads / NumOfFlips) * 100);
+                Console.WriteLine("Percentage of heads is {0}", Percent_heads);
+
+                double Percent_tails = Math.Round((heads / NumOfFlips) * 100);
+                Console.WriteLine("Percentage of heads is {0}", Percent_tails);
+            }
+            else
+            {
+                Console.WriteLine("please enter a valid num");
+            }
+
+        }
+        
+    
+    }
+}
---
+++ FlipCoin/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace FlipCoin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Flip_Coin myobj = new Flip_Coin();
+            myobj.Heads_Tails();
+        }
+    }
+}
